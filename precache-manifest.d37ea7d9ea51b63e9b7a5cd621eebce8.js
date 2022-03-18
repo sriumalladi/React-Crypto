e30948ae6d335d74362baf4be9b4f859d523ca2b
sriumalladi <sriumalladi@users.noreply.github.com>
2022-03-18 14:58:16 +0530

    Add files via upload

---
+++ precache-manifest.d37ea7d9ea51b63e9b7a5cd621eebce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee28637f9e525affad524f297ca6e294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d8d5d700df1a3fa4779",
+    "url": "/static/css/2.137dce52.chunk.css"
+  },
+  {
+    "revision": "e337c91fc1507a5e9171",
+    "url": "/static/css/main.06fff707.chunk.css"
+  },
+  {
+    "revision": "5d8d5d700df1a3fa4779",
+    "url": "/static/js/2.8c8018bd.chunk.js"
+  },
+  {
+    "revision": "722806d1078d8c6017c22dead06a3162",
+    "url": "/static/js/2.8c8018bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e337c91fc1507a5e9171",
+    "url": "/static/js/main.6c3ae8a0.chunk.js"
+  },
+  {
+    "revision": "3f3ac9fe654cae80f741",
+    "url": "/static/js/runtime-main.8435eee7.js"
+  }
+]);
